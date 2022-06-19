62a9efb6c98230704febf28a1e292be503643c6b
Ethan <ysa4@aluno.ifal.edu.br>
2022-06-19 13:41:42 -0300

    integrando a nave ao cenário

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 import Animacao from './game/Animacao.js';
+import Teclado from './game/Teclado.js';
 import Fundo from './game/Fundo.js';
+import Nave from './game/Nave.js';
 
 
 const canvas = document.getElementById('canvas-animacao');
 const context = canvas.getContext('2d');
 
+const teclado = new Teclado(document);
+
 let carregadas = 0;
-let total = 3;
+let total = 4;
 
 const imgEspaco = new Image();
 imgEspaco.src = '../imgs/fundo/fundo-espaco.png';
@@ -17,20 +21,34 @@ imgEstrelas.src = '../imgs/fundo/fundo-estrelas.png';
 const imgNuvens = new Image();
 imgNuvens.src = '../imgs/fundo/fundo-nuvens.png';
 
+const imgNave = new Image();
+imgNave.src = '../imgs/nave.png';
+
 
 function iniciar() {
-    const fundoEspaco = new Fundo(context, imgEspaco);
-    const fundoEstrelas = new Fundo(context, imgEstrelas);
-    const fundoNuvens = new Fundo(context, imgNuvens);
-
-    fundoEspaco.velocidade = 3;
-    fundoEstrelas.velocidade = 7;
-    fundoNuvens.velocidade = 10;
-
     const animacao = new Animacao(context);
+
+    const fundoEspaco = new Fundo(context, imgEspaco);
+    fundoEspaco.velocidade = 3;
     animacao.novoSprite(fundoEspaco);
+
+    const fundoEstrelas = new Fundo(context, imgEstrelas);
+    fundoEstrelas.velocidade = 7;
     animacao.novoSprite(fundoEstrelas);
+
+    const fundoNuvens = new Fundo(context, imgNuvens);
+    fundoNuvens.velocidade = 10;
     animacao.novoSprite(fundoNuvens);
+
+    const nave = new Nave(context, teclado, imgNave);
+    nave.x = (canvas.width / 2) - (imgNave.width / 2);
+    nave.y = canvas.height - imgNave.height;
+    nave.velocidade = 8;
+    animacao.novoSprite(nave);
+
+    teclado.disparou(Teclado.ESPACO, function() {
+        nave.atirar();
+    });
 
     animacao.ligar();
 }
@@ -46,5 +64,6 @@ function carregando() {
 imgEspaco.onload = carregando;
 imgEstrelas.onload = carregando;
 imgNuvens.onload = carregando;
+imgNave.onload = carregando;
 
 
